c2e557943bbdb05cbb4257a8a6a1a9c0444e20f1
anandrajB <anand98.ar@gmail.com>
2024-03-26 13:54:46 +0530

    updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -58,7 +58,7 @@ const Footer = () => {
     return (
         <footer id="footer" className="bg-slate-900 ">
             <RevealOnScroll>
-                <div className="mx-auto max-w-7xl px-6 py-11 md:flex md:items-center md:justify-between lg:px-8">
+                <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
                     <div className="flex justify-center space-x-6 md:order-2">
                         {navigation.map((item) => (
                             <a key={item.name} href={item.href} className="text-gray-400 cursor-pointer hover:text-gray-500 ">
