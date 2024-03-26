2ca87422e0ab997e372a0c41e96714e68f81106c
anandrajB <anand98.ar@gmail.com>
2024-03-26 16:41:05 +0530

    updated about

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import Work from './Components/Work'
 import Project from './Components/Project'
 import Testimonals from './Components/Testimonals'
 import Footer from './Components/Footer'
+import About from './Components/About'
 
 const App = () => {
   return (
     <div>
       <Sidenav />
       <Main />
+      <About />
       <Work />
       <Project />
       <Testimonals />
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -20,7 +20,7 @@ const Main = () => {
                     <h1 className='sm:text-7xl font-semibold text-5xl text-white'>Anand Raj</h1>
                     <h1 className='font-sans bg-slate-950 sm:text-4xl text-blue-500 font-col text-1xl font-normal p-2 mt-5'>Software Engineer</h1>
                     <div className="mt-5 flex items-center gap-x-4">
-                        <a href='#work'><button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Explore</button></a>
+                        <a href='#about'><button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Explore</button></a>
                         <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
                             Hire Me <span className="inline-block ml-1"><CiShare1 size={14} /></span>
                         </button>
