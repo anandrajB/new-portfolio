0f282bb96041320df9cd088755b28a999b0b68ef
anandrajB <anand98.ar@gmail.com>
2024-06-26 22:30:45 +0530

    updated core

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,7 +3,7 @@ import RevealOnScroll from './Reveal'
 import anand from "../assets/anand.png";
 
 
-const Content = "I'm a Experienced backend developer specializing in crafting robust and scalable systems. Proficient in Python, Django, FastAPI, and AWS, I am dedicated to delivering top-tier software solutions. With over 2 years and 6 months of industry experience, I thrive on overcoming complex technical obstacles and am committed to excellence in every project."
+const Content = "I'm a Experienced backend developer specializing in crafting robust and scalable systems. Proficient in Python, Django, FastAPI, AWS  and React , I am dedicated to delivering top-tier software solutions. With over 3 years of industry experience in working with fintech platform , I thrive on overcoming complex technical obstacles and am committed to excellence in every project."
 const About = () => {
     return (
         <div id="about" className="relative bg-slate-900 py-10 sm:py-10">
