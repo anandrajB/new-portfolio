61429c95b4e93365d552eadd676651fc5b25493b
anandrajB <anand98.ar@gmail.com>
2024-10-05 23:23:46 +0530

    updated core

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import header from "../assets/header2.jpeg";
 import { CiShare1 } from "react-icons/ci";
-
-
+import resume from '../assets/ANAND_RAJ_B_RESUME.pdf'
 const navigation = [
 
     {
@@ -54,6 +53,9 @@ const navigation = [
 
 
 const Main = () => {
+
+
+
     return (
         <div id="main" className='w-full overflow-y-auto  h-screen bg-no-repeat bg-cover'
             style={{
@@ -96,11 +98,12 @@ const Main = () => {
                             <button type="button" class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
                                 Explore</button>
                         </a>
-                        <a href='https://www.canva.com/design/DAGRYTYOVBA/5JXsCa6KD4yinqfW8_GSpw/view?utm_content=DAGRYTYOVBA&utm_campaign=designshare&utm_medium=link&utm_source=editor' download>
+                        <a href={resume} download>
                             <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ">
                                 Download Resume
                             </button>
                         </a>
+
 
 
                         <a href='https://www.linkedin.com/in/anandraj98/' target='_blank'>
