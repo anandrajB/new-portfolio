f03327f1705e9a0513e1ec5df12750e0f1274198
anandrajB <anand98.ar@gmail.com>
2026-07-08 01:08:32 +0530

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
 
     devToolbar: {
         enabled: true
-    }
+    },
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true, // set to false when using @vercel/analytics@1.4.0
+        },
+    }),
 });
